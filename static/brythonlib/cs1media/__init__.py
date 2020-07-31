5f69627078c6f0879cf6e09c6868537b4f99644b
Jeongmin Byun <jmbyun91@gmail.com>
2020-07-31 18:28:24 +0900

    add skeleton for cs1media

---
+++ static/brythonlib/cs1media/__init__.py
@@ -0,0 +1,11 @@
+
+
+def create_picture(width, height, color=(0,0,0)):
+    if width < 0 or height < 0:
+        raise ValueError('Invalid image dimensions: %s, %s' % (width, height))
+    
+
+
+__all__ = [
+
+]
