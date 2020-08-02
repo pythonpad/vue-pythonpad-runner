1b5f30db173b20c61b1cbedd3fbb1c6ae0e48125
Jeongmin Byun <jmbyun91@gmail.com>
2020-08-02 15:56:31 +0900

    refactor cs1media to use native JS code for better performance

--- static/brythonlib/cs1media/__init__.py
+++ static/brythonlib/cs1media/__init__.py
@@ -17,6 +17,21 @@ def load_picture(filename=None):
         raise ValueError('Pre-extracted image data is not found. Be aware that cs1media in Pythonpad only supports loading an image file that already existed in pad\'s virtual file structure when the code is executed, only when cs1media is directly imported in main.py.')
     return Picture(file_dict['width'], file_dict['height'], data=file_dict['imageData'])
 
+# def create_picture(width, height, color=(0,0,0)):
+#     if width < 0 or height < 0:
+#         raise ValueError('Invalid image dimensions: %s, %s' % (width, height))
+#     return Picture.new(width, height, None, list(color))
+
+# def load_picture(filename=None):
+#     if filename is None:
+#         raise NotImplementedError('Pythonpad\'s cs1media does not support dynamic image file loading.')
+#     if not browser.self.isFileExist(filename):
+#         raise FileNotFoundError('No such file: \'%s\'' % filename)
+#     file_dict = browser.self.getFileDict(filename)
+#     if 'imageData' not in file_dict:
+#         raise ValueError('Pre-extracted image data is not found. Be aware that cs1media in Pythonpad only supports loading an image file that already existed in pad\'s virtual file structure when the code is executed, only when cs1media is directly imported in main.py.')
+#     return Picture.new(file_dict['width'], file_dict['height'], file_dict['imageData'], None)
+
 
 __all__ = [
     'create_picture',
--- static/brythonlib/cs1media/picture.py
+++ static/brythonlib/cs1media/picture.py
@@ -2,62 +2,125 @@ import browser
 import json
 from .color import Color
 
+# def sanitize_color(color):
+#     if isinstance(color, str):
+#         return getattr(Color, color)
+#     else:
+#         return color
+
 def sanitize_color(color):
     if isinstance(color, str):
-        return getattr(Color, color)
+        return list(getattr(Color, color))
     else:
         return color
 
+browser.self.sanitizeColor = sanitize_color
+
+browser.self.eval('''
+class Picture {
+    constructor(width, height, data, color) {
+        this.width = width
+        this.height = height
+        this.title = ''
+        const buffer = new ArrayBuffer(width * height * 4);
+        this.data = new Uint8ClampedArray(buffer);
+        if (data) {
+            this.data.set(data, 0)
+        } else {
+            const safeColor = color ? self.sanitizeColor(color) : [0, 0, 0]
+            for (let i = 0; i < (width * height); i++) {
+                this.data.set(safeColor, i * 4)
+            }
+        }
+        for (let i = 0; i < (width * height); i++) {
+            this.data[(i * 4) + 3] = 255
+        }
+    }
+
+    show1() {
+        return this.show()
+    }
+
+    show() {
+        self.sendMsg('screen.cs1media.show', {
+            width: this.width,
+            height: this.height,
+            imageData: this.data
+        })
+    }
+
+    setPixels(color) {
+        const safeColor = color ? self.sanitizeColor(color) : [0, 0, 0]
+        for (let i = 0; i < (width * height); i++) {
+            this.data.set(safeColor, i * 4)
+        }
+    }
+
+    setTitle(title) {
+        this.title = title
+    }
+
+    title() {
+        return this.title
+    }
+
+    size() {
+        return [this.width, this.height]
+    }
+
+    getIndex(x, y) {
+        return (y * this.width * 4) + (x * 4)
+    }
+
+    get(x, y) {
+        const i = this.getIndex(x, y)
+        const subarray = this.data.subarray(i, i + 3)
+        return [subarray[0], subarray[1], subarray[2]]
+    }
+
+    set(x, y, color) {
+        const safeColor = self.sanitizeColor(color)
+        const i = this.getIndex(x, y)
+        this.data.set(safeColor, i)
+    }
+
+    greet() {
+        return 'hello'
+    }
+}
+self.Picture = Picture
+''')
+
 class Picture(object):
     def __init__(self, width, height, data=None, color=(0,0,0)):
-        self.width = width
-        self.height = height
-        self.title = ''
-        if data is not None:
-            self.data = data
-        else:
-            pixel = [v for v in sanitize(color)] + [1]
-            self.data = pixel * (width * height)
+        self.obj = browser.self.Picture.new(width, height, data, list(color))
 
     def size(self):
-        return self.width, self.height
+        return self.obj.size()
 
     def show1(self):
-        return self.show()
+        return self.obj.show1()
 
     def show(self):
-        print('Rendering the picture...')
-        picture_dict = {
-            'width': self.width,
-            'height': self.height,
-            'imageData': self.data,
-        }
-        browser.self.sendMsg('screen.cs1media.show', picture_dict)
-        print('Done.')
+        return self.obj.show()
 
     def set_pixels(self, color=(0,0,0)):
-        pixel = [v for v in sanitize(color)] + [1]
-        self.data = pixel * (self.width * self.height)
+        return self.obj.setPixels(list(color))
 
     def set_title(self, title):
-        self.title = title
+        return self.obj.setTitle()
 
     def title(self):
-        return self.title
-
-    def get_index(self, x, y):
-        return (y * self.width * 4) + (x * 4)
+        return self.obj.title()
 
     def get(self, x, y):
-        start = self.get_index(x, y)
-        return self.data[start], self.data[start + 1], self.data[start + 2]
+        return tuple(self.obj.get(x, y))
 
     def set(self, x, y, color):
-        r, g, b = sanitize_color(color)
-        start = self.get_index(x, y)
-        self.data[start] = r
-        self.data[start + 1] = g
-        self.data[start + 2] = b
+        if isinstance(color, str):
+            return self.obj.set(x, y, color)
+        else:
+            return self.obj.set(x, y, list(color))
 
     def save_as(self, filename=None):
         raise NotImplementedError('cs1media in Pythonpad does not support save_as method.')
