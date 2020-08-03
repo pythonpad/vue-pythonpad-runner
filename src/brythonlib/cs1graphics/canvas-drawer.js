85ad0a14c0a1ed08bad56cf0ce4646a17e13a74b
Jeongmin Byun <jmbyun91@gmail.com>
2020-08-03 15:33:36 +0900

    support rectangles in cs1graphics

--- src/brythonlib/cs1graphics/canvas-drawer.js
+++ src/brythonlib/cs1graphics/canvas-drawer.js
@@ -1,3 +1,5 @@
+const SVGNS = 'http://www.w3.org/2000/svg'
+
 export default class CanvasDrawer {
     constructor(containerElement) {
         this.containerElement = containerElement
@@ -5,24 +7,100 @@ export default class CanvasDrawer {
         this.svgElement = null
         this.canvasObject = null
         this.objects = {}
+        this.elements = {}
+        this.initContainer()
+    }
+
+    initContainer() {
+        const box = document.createElement('div')
+        box.style.width = '100%'
+        box.style.height = '100%'
+        box.style.backgroundColor = '#ffffff'
+        box.style.display = 'flex'
+        box.style.alignItems = 'center'
+        box.style.justifyContent = 'center'
+        this.innerContainerElement = box
+        this.containerElement.appendChild(box)
     }
 
     sanitizeColor(color) {
+        if (Array.isArray(color)) {
+            if (color.length === 3) {
+                return `rgb(${color.join(',')})`
+            } else if (color.length === 4) {
+                return `rgba(${color.join(',')})`
+            }
+        }
         return color
     }
 
     onCreateCanvas(task) {
-        this.objects[task.canvas.id] = task.canvas
         this.canvasObject = task.canvas
-        this.svgElement = document.createElement('svg')
-        this.svgElement.xmlns = 'http://www.w3.org/2000/svg'
-        this.svgElement.width = task.canvas.width
-        this.svgElement.height = task.canvas.height
-        this.svgElement.style.backgroundColor = task.canvas.bgColor
-        this.containerElement.appendChild(this.svgElement)
+        this.svgElement = document.createElementNS(SVGNS, 'svg')
+        this.svgElement.setAttributeNS(null, 'viewBox', `0 0 ${task.canvas.width} ${task.canvas.height}`)
+        this.svgElement.setAttributeNS(null, 'preserveAspectRatio', 'xMidYMid meet')
+        const styles = {
+            'width': `${task.canvas.width}px`,
+            'height': `${task.canvas.height}px`,
+            'max-width': '100%',
+            'max-height': '100%',
+            'flex': '0 0 auto',
+            'background-color': this.sanitizeColor(task.canvas.bgColor),
+        }
+        const styleString = Object.entries(styles).map(kv => `${kv[0]}:${kv[1]}`).join(';')
+        this.svgElement.setAttributeNS(null, 'style', styleString)
+        this.innerContainerElement.appendChild(this.svgElement)
+        this.objects[task.canvas.id] = task.canvas
+        this.elements[task.canvas.id] = this.svgElement
+    }
+
+    getTagNameByDrawableType(dtype) {
+        switch (dtype) {
+            case 'rectangle':
+                return 'rect'
+        
+            default:
+                return 'rect'
+        }
+    }
+
+    setDrawableAttributes(el, drawable) {
+        el.setAttributeNS(null, 'x', drawable.x - (drawable.width / 2))
+        el.setAttributeNS(null, 'y', drawable.y - (drawable.height / 2))
+        el.setAttributeNS(null, 'height', drawable.height)
+        el.setAttributeNS(null, 'width', drawable.width)
+        el.setAttributeNS(null, 'fill', this.sanitizeColor(drawable.fillColor))
+        el.setAttributeNS(null, 'stroke', this.sanitizeColor(drawable.borderColor))
+        el.setAttributeNS(null, 'stroke-width', this.sanitizeColor(drawable.borderWidth))
+        const transforms = []
+        if (drawable.rotateAngle !== 0) {
+            const rotatex = drawable.x + drawable.refx
+            const rotatey = drawable.y + drawable.refy
+            transforms.push(`rotate(${drawable.rotateAngle} ${rotatex} ${rotatey})`)
+        }
+        if (transforms) {
+            el.setAttributeNS(null, 'transform', transforms.join(' '))
+        }
+        // scaleFactor, flipAngle, depth, centerx, centery
+    }
+
+    onAdd(task) {
+        const tagName = this.getTagNameByDrawableType(task.drawable.type)
+        const el = document.createElementNS(SVGNS, 'rect')
+        el.id = `d${task.drawable.id}`
+        this.setDrawableAttributes(el, task.drawable)
+        this.elements[task.container_id].appendChild(el)
+        this.objects[task.drawable.id] = task.drawable
+        this.elements[task.drawable.id] = el
+    }
+
+    onEdit(task) {
+        const el = this.elements[task.drawable.id]
+        this.setDrawableAttributes(el, task.drawable)
     }
 
     onTask(task) {
+        console.log('TASK', task)
         switch(task.task) {
             case 'create_canvas':
                 this.onCreateCanvas(task)
@@ -30,6 +108,10 @@ export default class CanvasDrawer {
 
             case 'add':
                 this.onAdd(task)
+                break
+
+            case 'edit':
+                this.onEdit(task)
                 break
 
             default:
--- src/brythonlib/cs1graphics/index.js
+++ src/brythonlib/cs1graphics/index.js
@@ -2,22 +2,7 @@ import CanvasDrawer from './canvas-drawer';
 
 export default class CanvasDrawHelper {
     constructor(el) {
-        const box = document.createElement('div')
-        box.style.width = '100%'
-        box.style.height = '100%'
-        box.style.backgroundColor = '#ffffff'
-        box.style.display = 'flex'
-        box.style.alignItems = 'center'
-        box.style.justifyContent = 'center'
-        const innerBox = document.createElement('div')
-        innerBox.width = p.width // ???
-        innerBox.height = p.height
-        innerBox.style.maxWidth = '100%'
-        innerBox.style.maxHeight = '100%'
-        innerBox.style.flex = '0 0 auto'
-        box.appendChild(innerBox)
-        el.appendChild(box)
-        this.drawer = new CanvasDrawer(innerBox)
+        this.drawer = new CanvasDrawer(el)
     }
 
     draw(msg) {
